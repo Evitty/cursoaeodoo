4aefdbd998673f92d22b314e1b22dd3aa42e8f79
lucaselvira <lucaselviraluque@gmail.com>
2026-03-02 10:49:47 +0100

    update

---
+++ music_school_eva/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Music School',
+    'version': '18.0.0.0.0',
+    'description': 'Manages a music school with students , teachers and classes.',
+    'summary': 'Manages a music school with students, teachers and classes.',
+    'author': 'Eva del Barco Duro',
+    'license': 'LGPL-3',
+    'category': 'Music School',
+    'depends': [
+        'base'
+    ],
+}
